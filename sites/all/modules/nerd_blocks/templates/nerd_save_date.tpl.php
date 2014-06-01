7ad858271b638090acc888acaa8b983986bd37cc
Jeff Landfried <jlandfri@gmail.com>
2014-05-31 21:05:03 -0400

    Add "save the date" block

---
+++ sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
@@ -0,0 +1,7 @@
+<div class="nerd-sign-up-content">
+  <h4>September 17-19</h4>
+  <?php print $nerd_img;?>
+  <p>Learn with us at a free three day conference of talks, workshops, coding challenges, food and fun!</p>
+  <a class="purple-button" href="<?php print $register_path; ?>"><?php print $find_out_more_text; ?></a>
+  <a class="purple-button" href="<?php print $register_path; ?>"><?php print $register_text; ?></a>
+</div>
