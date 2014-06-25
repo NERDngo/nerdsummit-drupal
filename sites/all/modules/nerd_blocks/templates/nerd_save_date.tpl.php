3b0e8045997087aa5315492b89dc54ac6474d45c
jayme johnson <jahaimon@gmail.com>
2014-06-25 14:52:46 -0400

    corrected dates on nerd block

--- sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
+++ sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
@@ -1,5 +1,5 @@
 <div class="nerd-sign-up-content">
-  <h4>September 17-19</h4>
+  <h4>September 12-14</h4>
   <?php print $nerd_img;?>
   <p>Learn with us at a free three day conference of talks, workshops, coding challenges, food and fun!</p>
   <a class="purple-button" href="<?php print $register_path; ?>"><?php print $find_out_more_text; ?></a>
