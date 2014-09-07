2f47315739000ad5e4d69a5d6974df87fe29f028
Kelly Albrecht <kelly@lastcallmedia.com>
2014-09-06 21:19:30 -0400

    save the date 2015

--- sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
+++ sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
@@ -1,5 +1,5 @@
 <div class="nerd-sign-up-content">
-  <h4>September 12-14</h4>
+  <h4>Fall 2015</h4>
   <?php print $nerd_img;?>
   <p>Learn with us at a free three day conference of talks, workshops, coding challenges, food and fun!</p>
   <?php print $find_out_more; ?>
