d970ac4982f7461c6bef98f8e19a1807758549a5
Kelly Albrecht <mail@ksadev.com>
2016-05-05 01:24:28 -0400

    Winter 2016

--- sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
+++ sites/all/modules/nerd_blocks/templates/nerd_save_date.tpl.php
@@ -1,7 +1,7 @@
 <div class="nerd-sign-up-content">
-  <h4>Fall 2015</h4>
+  <h4>Winter 2017</h4>
   <?php print $nerd_img;?>
-  <p>Learn with us at a free three day conference of talks, workshops, coding challenges, food and fun!</p>
+  <p>Learn with us at a free conference of talks, workshops, coding challenges, food and fun!</p>
   <?php print $find_out_more; ?>
   <?php print $register; ?>
 </div>
